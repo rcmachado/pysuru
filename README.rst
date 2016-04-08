55e96d413c59c4ac6e64c4cdfd2d855e7e7cb699
Rodrigo Machado <rodrigo.machado@corp.globo.com>
2016-04-08 16:21:56 -0300

    Initial structure

---
+++ README.rst
@@ -0,0 +1,25 @@
+pysuru
+======
+
+Python lib to access Tsuru API.
+
+Install
+-------
+
+.. code:: shell
+
+    pip install psuru
+
+Works on Python 2.7 and 3.5.
+
+Developing
+----------
+
+.. code:: shell
+
+    pip install -r requirements.txt
+
+License
+-------
+
+MIT. See [LICENSE](LICENSE).
---
+++ setup.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+from setuptools import setup
+
+
+setup(
+    name='pysuru',
+    version='0.0.1',
+    description='Python library to interact with Tsuru API',
+    long_description=open('README.rst', 'r').read(),
+    keywords='tsuru',
+    author='Rodrigo Machado',
+    author_email='rcmachado@gmail.com',
+    url='https://github.com/rcmachado/pysuru',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers'
+        'Topic :: Software Development :: Libraries',
+    ],
+    install_requires=[
+        'urllib3>=1.15'
+    ]
+    packages=['pysuru'],
+    platforms=['linux', 'osx']
+)
+
