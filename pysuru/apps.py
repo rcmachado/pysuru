78d2c41bf38b4900d2d9e38fd83c33f382efd0ff
Rodrigo Machado <rodrigo.machado@corp.globo.com>
2016-04-14 09:54:10 -0300

    Improvements to AppsAPI

--- pysuru/apps.py
+++ pysuru/apps.py
@@ -3,23 +3,23 @@ import json
 from builtins import tuple as _tuple
 from collections import namedtuple
 
-from pysuru.base import BaseAPI
+from pysuru.base import BaseAPI, ObjectMixin
+from pysuru.services import ServiceInstanceAPI
 
 
 app_attrs = (
     'name',
     'description',
     'platform',
+    'team_owner',
     'pool',
 )
 
 
-class App(namedtuple('App', app_attrs)):
-    @classmethod
-    def create(cls, **kwargs):
-        """Remove any unknown field from ``data`` dictionary and return an instance"""
-        attrs = {k: kwargs.get(k, None) for k in app_attrs}
-        return cls(**attrs)
+class App(namedtuple('App', app_attrs), ObjectMixin):
+    def services(self):
+        api = ServiceInstanceAPI(self.target, self.token)
+        return api.filter_by_app(self, self.name)
 
 
 class AppsAPI(BaseAPI):
@@ -29,7 +29,7 @@ class AppsAPI(BaseAPI):
     def data(self):
         if not self._data:
             http_response = self.request('GET', '/apps', headers=self.headers)
-            response = json.loads(http_response.read())
+            response = json.loads(http_response.data.decode('utf-8'))
             for data in response:
                 self._data.append(App.create(**data))
         return self._data
@@ -43,9 +43,50 @@ class AppsAPI(BaseAPI):
     def __getitem__(self, key):
         return self.data[key]
 
+    def get(self, name):
+        http_response = self.request('GET', '/apps/{}'.format(name))
+        if http_response.status == 404:
+            raise AppDoesNotExists('App {} not found'.format(name))
+
+        if http_response.status != 200:
+            return False
+
+        response = json.loads(http_response.data.decode('utf-8'))
+        data = {
+            'name': response['name'],
+            'description': response['description'],
+            'plan': response['plan']['name'],
+            'pool': response['pool'],
+            'team_owner': response['teamowner'],
+            'platform': response['platform'],
+        }
+        return App.create(**data)
+
+    def add(self, data):
+        http_response = self.post_json('/apps', data)
+        if http_response.status == 200:
+            return True
+        elif http_response.status == 409:
+            raise
+        return False
+
     def update(self, name, data):
         http_response = self.post_json('/apps/{}'.format(name), data)
         if http_response.status == 200:
             return True
         else:
             return False
+
+    def bind_service(self, name, service_instance):
+        path = '/services/{}/instances/{}/{}'.format(service_instance.type, service_instance.name, name)
+        http_response = self.request('PUT', path)
+        if http_response.status == 200:
+            return True
+        return False
+
+
+class AppAlreadyExists(Exception):
+    pass
+
+class AppDoesNotExists(Exception):
+    pass
