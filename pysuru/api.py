58b2bfd34f9699c88ebb04ae4253f90392393c98
Rodrigo Machado <rodrigo.machado@corp.globo.com>
2016-04-14 09:53:39 -0300

    Add ServiceInstanceAPI

---
+++ pysuru/api.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+"""
+Public endpoint to import API classes
+
+Instead of importing each module individually (eg.
+``from pysuru.apps import AppsAPI``), import from this module.
+"""
+from __future__ import absolute_imports
+
+from .apps import AppsAPI
+from .services import ServicesAPI
---
+++ pysuru/services.py
@@ -0,0 +1,51 @@
+# coding: utf-8
+import json
+from collections import namedtuple
+
+from pysuru.base import BaseAPI, ObjectMixin
+
+
+SERVICE_INSTANCE_ATTRS = (
+    'name',
+    'description',
+    'type',
+    'plan',
+    'teamOwner',
+)
+
+
+_ServiceInstance = namedtuple('ServiceInstance', SERVICE_INSTANCE_ATTRS)
+
+
+class ServiceInstance(_ServiceInstance, ObjectMixin):
+    pass
+
+
+class ServiceInstanceAPI(BaseAPI):
+    def filter_by_app(self, name):
+        http_response = self.request('GET', '/services/instances?app=' + name)
+        response = json.loads(http_response.data.decode('utf-8'))
+        services = []
+        for service_data in response:
+            for index, instance in enumerate(service_data['instances']):
+                data = {
+                    'name': instance,
+                    'type': service_data['service'],
+                    'plan': service_data['plans'][index],
+                }
+                services.append(ServiceInstance.create(**data))
+        return services
+
+    def add(self, data):
+        http_response = self.post_json('/services/instances', data)
+        response = json.loads(http_response.data.decode('utf-8'))
+        if response.status == 409:
+            raise ServiceAlreadyExists()
+        elif response.status == 200:
+            return True
+        else:
+            return False
+
+
+class ServiceAlreadyExists(Exception):
+    pass
