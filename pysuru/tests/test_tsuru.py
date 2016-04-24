b60835c68162b75571ab7525893906141d2bf79f
Rodrigo Machado <rodrigo.machado@corp.globo.com>
2016-04-23 21:42:16 -0300

    Consider env vars when creating object
    Try to read credentials from env variables when they're not provided.

--- pysuru/tests/test_tsuru.py
+++ pysuru/tests/test_tsuru.py
@@ -1,10 +1,45 @@
 # coding: utf-8
+import os
+
 try:
     from unittest import mock
 except ImportError:
     import mock
 
+import pytest
+
 from pysuru.tsuru import Tsuru
+
+
+def test_create_object_with_credentials_from_arguments():
+    os.environ['TSURU_TARGET'] = 'target'
+    os.environ['TSURU_TOKEN'] = 'token'
+
+    obj = Tsuru('target', 'token')
+    assert obj.target == 'target'
+    assert obj.token == 'token'
+
+
+def test_create_object_with_credentials_from_env_variables():
+    os.environ['TSURU_TARGET'] = 'target'
+    os.environ['TSURU_TOKEN'] = 'token'
+
+    obj = Tsuru()
+    assert obj.target == 'target'
+    assert obj.token == 'token'
+
+
+def test_create_object_without_credentials_should_raise_error():
+    os.environ['TSURU_TARGET'] = ''
+    os.environ['TSURU_TOKEN'] = ''
+
+    with pytest.raises(ValueError) as excinfo:
+        obj = Tsuru(None, 'something')
+    assert 'target' in str(excinfo.value)
+
+    with pytest.raises(ValueError) as excinfo:
+        obj = Tsuru('something', None)
+    assert 'token' in str(excinfo.value)
 
 
 @mock.patch('pysuru.tsuru.AppsAPI')
--- pysuru/tsuru.py
+++ pysuru/tsuru.py
@@ -1,9 +1,37 @@
 # coding: utf-8
+import os
+
 from pysuru.apps import AppsAPI
 
 
 class Tsuru(object):
-    def __init__(self, target, token):
+    """
+    Main class to interact with Tsuru API
+    """
+
+    def __init__(self, target=None, token=None):
+        """
+        Creates an object and stores credentials used to connect to
+        Tsuru API.
+
+        Receives ``target`` and ``token`` or, if not provided, read
+        ``TSURU_TARGET`` and ``TSURU_TOKEN`` env variables. If neither
+        options are provided, raises ValueError exception.
+        """
+        if not target:
+            target = os.environ.get('TSURU_TARGET', None)
+            if not target:
+                raise ValueError(
+                    'Missing target argument. Either provide target param or '
+                    'set TSURU_TARGET env variable.')
+
+        if not token:
+            token = os.environ.get('TSURU_TOKEN', None)
+            if not token:
+                raise ValueError(
+                    'Missing token argument. Either provide token param or '
+                    'set TSURU_TOKEN env variable.')
+
         self.target = target
         self.token = token
 
