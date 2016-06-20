6c54fc230e8c889a2351f20b524382a5c6e29d1c
Rodrigo Machado <rodrigo.machado@corp.globo.com>
2016-06-20 13:12:20 -0300

    Update examples to match docs
    Use the interface defined in the docs in the examples scripts.

--- examples/apps.py
+++ examples/apps.py
@@ -9,22 +9,19 @@ TSURU_TARGET = os.environ.get('TSURU_TARGET', None)
 TSURU_TOKEN = os.environ.get('TSURU_TOKEN', None)
 
 if not TSURU_TARGET or not TSURU_TOKEN:
-    print('You must set TSURU_TARGET and TSURU_TOKEN.')
+    print('You must set TSURU_TARGET and TSURU_TOKEN env variables.')
     sys.exit(1)
 
-api = TsuruClient(TSURU_TARGET, TSURU_TOKEN)
+# Creating TsuruClient instance
+tsuru = TsuruClient(TSURU_TARGET, TSURU_TOKEN)
 
-# List all apps that this token has access to
-for app in api.apps:
-    print(app.name)
-
-# Update one specific app
-api.apps.update('my-awesome-app', {'description': 'My awesome app'})
+# List all apps that this user has access to
+for app in tsuru.apps.list():
+    print('App: {}'.format(app.name))
 
 # Get information for one app
-app = App.get('my-awesome-app')
-print('%s: %s' % (app.name, app.description))
+app = tsuru.apps.get('my-awesome-app')
+print('{app.name}: {app.description}'.format(app=app))
 
-# List all services instances for app
-for service in app.services:
-    print('Service: %s' % service.name)
+# Update specific app
+tsuru.apps.update('my-awesome-app', {'description': 'My new awesome description'})
