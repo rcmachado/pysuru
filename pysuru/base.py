94f59b2146f3bf1756f5c6c215900645758ee68f
Rodrigo Machado <rodrigo.machado@corp.globo.com>
2016-06-20 13:15:54 -0300

    Defines a basic interface for API classes

--- pysuru/base.py
+++ pysuru/base.py
@@ -1,7 +1,27 @@
 # coding: utf-8
 class BaseAPI(object):
+    """
+    Base class for API related classes
+
+    Provides basic skeleton for API related classes
+    """
     def __init__(self, client):
         self.client = client
+
+    def list(self, *args, **kwargs):
+        raise NotImplemented('Object list not implemented')
+
+    def get(self, name):
+        raise NotImplemented('Object get not implemented')
+
+    def create(self, data):
+        raise NotImplemented('Object creation not implemented')
+
+    def update(self, name, data):
+        raise NotImplemented('Object update not implemented')
+
+    def delete(self, name):
+        raise NotImplemented('Object deletion not implemented')
 
 
 class ObjectMixin(object):
