3538e32c551f7c600e266d9850e530e74af8a920
Rodrigo Machado <rodrigo.machado@corp.globo.com>
2016-06-20 13:19:38 -0300

    Change interface for querying service instance API
    Use the normalized method names.

--- pysuru/services.py
+++ pysuru/services.py
@@ -5,25 +5,17 @@ from collections import namedtuple
 from pysuru.base import BaseAPI, ObjectMixin
 
 
-SERVICE_INSTANCE_ATTRS = (
-    'name',
-    'description',
-    'type',
-    'plan',
-)
+_service_instance = namedtuple('ServiceInstance',
+                               ('name', 'description', 'type', 'plan'))
 
 
-_ServiceInstance = namedtuple('ServiceInstance', SERVICE_INSTANCE_ATTRS)
-
-
-class ServiceInstance(_ServiceInstance, ObjectMixin):
+class ServiceInstance(_service_instance, ObjectMixin):
     pass
 
 
 class ServiceInstanceAPI(BaseAPI):
-    def filter_by_app(self, name):
-        path = '/services/instances?app={}'.format(name)
-        _, response = self.client.get(path)
+    def list(self, app_name):
+        _, response = self._filter_by_app_name(app_name)
 
         services = []
         for service_data in response:
@@ -36,18 +28,31 @@ class ServiceInstanceAPI(BaseAPI):
                 services.append(ServiceInstance.create(self.client, **data))
         return services
 
-    def add(self, data):
+    def _filter_by_app_name(self, app_name):
+        path = '/services/instances?app={}'.format(app_name)
+        return self.client.get(path)
+
+    def create(self, data):
         body = json.dumps(data)
         http_response = self.client.urlopen('POST', '/services/instances',
                                             body=body)
         response = json.loads(http_response.data.decode('utf-8'))
-        if response.status == 409:
-            raise ServiceAlreadyExists()
-        elif response.status == 200:
+        if response.status == 200:
+            # TODO: return ServiceInstance instance
             return True
-        else:
-            return False
+
+        if response.status == 409:
+            raise ServiceInstanceAlreadyExists(
+                'Service instance {} already exists'.format(data['name']))
+
+        raise ServiceInstanceError(
+            'Unknown error when creating service instance {}'
+            .format(data['name']))
 
 
-class ServiceAlreadyExists(Exception):
+class ServiceInstanceError(Exception):
+    pass
+
+
+class ServiceInstanceAlreadyExists(ServiceInstanceError):
     pass
