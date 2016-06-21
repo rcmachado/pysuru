fd9c8f8695c47078b76de27ea12c04d8de448951
Rodrigo Machado <rodrigo.machado@corp.globo.com>
2016-06-21 20:07:45 -0300

    Fix AppsAPI.update test

--- pysuru/tests/test_apps.py
+++ pysuru/tests/test_apps.py
@@ -24,12 +24,12 @@ def test_appsapi_all_should_return_list_of_apps(tsuru_apps_list):
 
 def test_update_should_make_request_with_correct_payload():
     client = mock.MagicMock()
-    client.urlopen.return_value.status = 200
+    client.put.return_value.status = 200
 
     payload = {'random-field': 'random-value'}
 
     apps = AppsAPI(client)
 
     assert apps.update('app-name', payload) is True
-    assert client.urlopen.call_args_list == [
-        mock.call('POST', '/apps/app-name', body=json.dumps(payload))]
+    assert client.put.call_args_list == [
+        mock.call('/apps/app-name', data=payload)]
